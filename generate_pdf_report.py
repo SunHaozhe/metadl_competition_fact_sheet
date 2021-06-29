f663cc07574afa77cdec76cee733128232888db8
ihsanullah2131 <ihsan2131@gmail.com>
2021-06-29 13:04:50 +0200

    HTML images are embedded in the html file now, independent of the source of image using base-64 format
    Template html is updated accordingly to adjust the base-64 images

--- generate_pdf_report.py
+++ generate_pdf_report.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import base64
 import jinja2
 import pdfkit
 
@@ -12,6 +13,7 @@ parser.add_argument("--results_dir", type=str, required=True,  help="""The direc
 parser.add_argument("--title", type=str, required=True, help="""Title of the html and pdf file.""")
 
 parser.add_argument("--output_dir", type=str, default="./", help="""The directory where the html and pdf file will be stored. Default is current directory""")
+
 parser.add_argument("--keep_html", action="store_true", default=False, 
     help="""Whether keep the html reports from which pdf reports can be built. 
     This is useful if the conversion between html and pdf fails.""")
@@ -71,7 +73,10 @@ categories_combined = logs[3].split(" : ")[1]
 #----------------------------
 # Overall Histogram
 #----------------------------
-over_all_auc_histogram = os.path.join(images_path, "overall_auc_histogram.png")
+over_all_auc_histogram_path = os.path.join(images_path, "overall_auc_histogram.png")
+with open(over_all_auc_histogram_path, "rb") as image_file:
+    over_all_auc_histogram = base64.b64encode(image_file.read()).decode('ascii')
+
 
 
 
@@ -119,39 +124,53 @@ for super_cat in super_categories:
     #---------------------------------
     # Accuracy and Loss Plot
     #---------------------------------
-    super_cat_dic['accuracy_loss_plot'] = os.path.join(images_path, super_cat, "train_results.png")
+    accuracy_loss_plot_path = os.path.join(images_path, super_cat, "train_results.png")
+    with open(accuracy_loss_plot_path, "rb") as image_file:
+        super_cat_dic['accuracy_loss_plot'] = base64.b64encode(image_file.read()).decode('ascii')
     
     #---------------------------------
     # Confusion Matrix
     #---------------------------------
-    super_cat_dic['confusion_matrix_plot'] = os.path.join(images_path, super_cat, "confusion_matrix.png")
+    confusion_matrix_plot_path = os.path.join(images_path, super_cat, "confusion_matrix.png")
+    with open(confusion_matrix_plot_path, "rb") as image_file:
+        super_cat_dic['confusion_matrix_plot'] = base64.b64encode(image_file.read()).decode('ascii')
     
     #---------------------------------
     # AUC Plot
     #--------------------------------
-    super_cat_dic['auc_plot'] = os.path.join(images_path, super_cat, "auc.png")
+    auc_plot_path = os.path.join(images_path, super_cat, "auc.png")
+    with open(auc_plot_path, "rb") as image_file:
+        super_cat_dic['auc_plot'] = base64.b64encode(image_file.read()).decode('ascii')
     
     #---------------------------------
     # AUC Histogram
     #--------------------------------
-    super_cat_dic['auc_histogram_plot'] = os.path.join(images_path, super_cat, "auc_histogram.png")
+    auc_histogram_plot_path = os.path.join(images_path, super_cat, "auc_histogram.png")
+    with open(auc_histogram_plot_path, "rb") as image_file:
+        super_cat_dic['auc_histogram_plot'] = base64.b64encode(image_file.read()).decode('ascii')
     
     #---------------------------------
     # ROC Curves
     #--------------------------------
-    super_cat_dic['roc_curves_plot'] = os.path.join(images_path, super_cat, "roc_curves.png")
+    roc_curves_plot_path = os.path.join(images_path, super_cat, "roc_curves.png")
+    with open(roc_curves_plot_path, "rb") as image_file:
+        super_cat_dic['roc_curves_plot'] = base64.b64encode(image_file.read()).decode('ascii')
     
     
     #---------------------------------
     # Sample Images
     #--------------------------------
-    super_cat_dic['sample_images'] = os.path.join(images_path, super_cat, "sample_images.png")
+    sample_images_path = os.path.join(images_path, super_cat, "sample_images.png")
+    with open(sample_images_path, "rb") as image_file:
+        super_cat_dic['sample_images'] = base64.b64encode(image_file.read()).decode('ascii')
     
     #---------------------------------
     # Wrongly Classified Images
     #--------------------------------
-    super_cat_dic['wrongly_classified_images'] = os.path.join(images_path, 
+    wrongly_classified_images_path = os.path.join(images_path, 
                                                               super_cat, "wrongly_classified_images.png")
+    with open(wrongly_classified_images_path, "rb") as image_file:
+        super_cat_dic['wrongly_classified_images'] = base64.b64encode(image_file.read()).decode('ascii')
     
     
     episodes.append(super_cat_dic)
